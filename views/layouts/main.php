e19139995fc75dc3f4d1988bf7ceed8b3d5b8fed
Rockman <gustavoandrade.net@gmail.com>
2016-09-20 14:31:03 -0300

    added icons

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -24,7 +24,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'Contax',
+        'brandLabel' => '<span class="glyphicon glyphicon-book" aria-hidden="true"></span> ' . 'Contax',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
@@ -32,12 +32,13 @@ AppAsset::register($this);
     ]);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
+        'encodeLabels' => false,
         'items' => [
-            ['label' => Yii::t('app', 'Contact'),  'url' => ['/contact/index'],'visible' => !Yii::$app->user->isGuest],
-            ['label' => Yii::t('app', 'Category'), 'url' => ['/category/index'],'visible' => !Yii::$app->user->isGuest],
-            ['label' => Yii::t('app', 'Options'),    'url' => ['/site/about'],'visible' => !Yii::$app->user->isGuest],
+            ['label' => '<span class="glyphicon glyphicon-th-list" aria-hidden="true"></span> ' . Yii::t('app', 'Contact'),  'url' => ['/contact/index'],'visible' => !Yii::$app->user->isGuest],
+            ['label' => '<span class="glyphicon glyphicon-flag" aria-hidden="true"></span> ' . Yii::t('app', 'Category'), 'url' => ['/category/index'],'visible' => !Yii::$app->user->isGuest],
+            ['label' => '<span class="glyphicon glyphicon-cog" aria-hidden="true"></span> ' . Yii::t('app', 'Options'),    'url' => ['/site/about'],'visible' => !Yii::$app->user->isGuest],
             Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
+                ['label' => '<span class="glyphicon glyphicon-lock" aria-hidden="true"></span> ' . 'Login', 'url' => ['/site/login']]
             ) : (
                 '<li>'
                 . Html::beginForm(['/site/logout'], 'post', ['class' => 'navbar-form'])
